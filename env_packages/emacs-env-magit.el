c420b56948909549502f6558a1d48f63edeb7f52
invi_MAC <invi.dh.kim@gmail.com>
2014-03-16 00:25:03 +0900

    no update this time

--- env_packages/emacs-env-magit.el
+++ env_packages/emacs-env-magit.el
@@ -1,6 +1,12 @@
+(defvar *_defp_git-modes* "git-modes")
 (defvar *_defp_magit* "magit")
 
+(add-to-list 'load-path (concat *_def_libraries_all_path* *_defp_git-modes*))
 (add-to-list 'load-path (concat *_def_libraries_all_path* *_defp_magit*))
 ;;(add-to-list 'load-path "~/.emacs.d/library/magit")
+
+(eval-after-load 'info
+  '(progn (info-initialize)
+          (add-to-list 'Info-directory-list (concat *_def_libraries_all_path* *_defp_magit*))))
 
 (require 'magit)
